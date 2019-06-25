ea4e7b38e31c48cb4ff1435b53645c3ee755fa56
AdrianaRosas <51386430+AdrianaRosas@users.noreply.github.com>
2019-06-25 15:29:59 +0200

    Cambios en main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 for(personajes in RICKANDMORTY.results){
-  console.log(RICKANDMORTY.results[personajes].origin.url);
+  console.log(RICKANDMORTY.results[personajes].image);
 }
 
 //console.log(RICKANDMORTY.results[0].image);
