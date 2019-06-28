2798f3232196fe5341bd8b6c14aef05d49452afb
AdrianaRosas <51386430+AdrianaRosas@users.noreply.github.com>
2019-06-28 03:38:53 +0200

    Subiendo avances en HTML y en el archivo main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+for(personajes in RICKANDMORTY.results){
+  console.log(RICKANDMORTY.results[personajes].origin.url);
+}
+//console.log(RICKANDMORTY.results[0].image);
+
+
+/*sirve para entender mejor el código, es como la clave del objeto. 
+no tiene valor en especial */
+/*for anidado para los strings de los episodios*/
