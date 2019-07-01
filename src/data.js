af2e3e6f077183cb2bdb3c65647e21a0da82d846
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-07-01 15:56:50 -0500

    agregué 2 center, en el primero tiene el botón que servirá para ordenar los pokemones por nombre, en el segundo van a aparecer los pokemones de uno en uno, con un botón para avanzar y otro para retroceder. Ya se puede acceder a la data y en data.js seleccioné los elementos de mi html que están dentro de mi segundo center para que se pinten en pantalla

--- src/data.js
+++ src/data.js
@@ -1,8 +1,23 @@
+//Esto ya estaba en el documento
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example;
+//---------------------------------------------------------------
+var firstButton = document.getElementById("principalButton");
+var p1 = document.getElementById("p1");
+let contador = 0;
+
+// const showName = () => {
+//   p1.innerHTML += window.pokemon.POKEMON.pokemon[2];
+// }
+firstButton.addEventListener("click", () => {
+  p1.innerHTML = window.pikachu.pokemon[contador].name;
+  contador = contador+1;
+})
+
+
