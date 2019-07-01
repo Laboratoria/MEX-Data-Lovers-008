257247669ec83f6680da38e3ffe7d646bf8317a1
Reyes Jimenez Adriana <boogiebasace@gmail.com>
2019-07-01 16:57:15 -0500

    Agregue parrafos vacios y los mande a llamar en data.js para pintar la data de pokemon.js

--- src/data.js
+++ src/data.js
@@ -8,16 +8,32 @@
 
 // window.example = example;
 //---------------------------------------------------------------
-var firstButton = document.getElementById("principalButton");
-var p1 = document.getElementById("p1");
+const firstButton = document.getElementById("principalButton");
+const name = document.getElementById("name");
+const img = document.getElementById("img");
+const type = document.getElementById("type");
+const height = document.getElementById("height");
+const weight= document.getElementById("weight");
+const candy =  document.getElementById("candy");
+const candy_count = document.getElementById("candy_count");
+const egg = document.getElementById("egg");
+const spawn_chance = document.getElementById("spawn_chance");
 let contador = 0;
 
 // const showName = () => {
 //   p1.innerHTML += window.pokemon.POKEMON.pokemon[2];
 // }
 firstButton.addEventListener("click", () => {
-  p1.innerHTML = window.pikachu.pokemon[contador].name;
-  contador = contador+1;
+  name.innerHTML = "Nombre:" + " " + window.pikachu.pokemon[contador].name;
+  img.src = window.pikachu.pokemon[contador].img;
+  type.innerHTML="Tipo:" + " " +  window.pikachu.pokemon[contador].type;
+  height.innerHTML="Altura:" + " " + window.pikachu.pokemon[contador].height;
+  weight.innerHTML="Ancho:" + " " +  window.pikachu.pokemon[contador].weight;
+  candy.innerHTML ="Caramelo:" + " " +  window.pikachu.pokemon[contador].candy;
+  candy_count.innerHTML = "Caramelos para evolucionar:" + " " + window.pikachu.pokemon[contador].candy_count;
+  egg.innerHTML = "Egg:" + " " + window.pikachu.pokemon[contador].egg;
+  spawn_chance.innerHTML = "spawn_chance:" + " " + window.pikachu.pokemon[contador].egg;
+  contador=contador+1;
 })
 
 
