847210a207a57b8889de171e1bd6770b62a8edef
SolGee <solsv00@hotmail.com>
2019-07-03 16:55:47 -0500

    Se agregaron comentarios de la función filtrar

--- src/data.js
+++ src/data.js
@@ -2,10 +2,11 @@
 // puedes ver como agregamos la función a nuestro objeto global window
 
 
-const filterData = (key, value) => {
-    let items = data.filter(element => element[key].indexOf(value) > -1)
-    console.log(items);
-    return items.map(element =>
+const filterData = (key, value) => {//2 parámetros. Value del index tiene 2 palabras (valor.propiedad)
+    let items = data.filter(element => element[key].indexOf(value) > -1)//Varaible vacia donde se guardara c/elemento que cumpla con la condición. -1 = index inexistente = false
+    return items.map(element => // items = array. .map itera items
+
+//Al cumplirse con la condición, ejecutar el siguiente código.
         `<div class="data-card">
         <div class="card">
             <div class="img">
@@ -22,4 +23,4 @@ const filterData = (key, value) => {
 }
 
 
-window.filterData = filterData;
+window.filterData = filterData; //Guarda la función en el global para poder ser invocada de donde sea
