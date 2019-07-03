a422e7a4eb284fb80b98c83877ed2971aa1d7855
JesssicaZetina <jessicazetina37@gmail.com>
2019-07-02 21:50:03 -0500

    data pintada

--- src/data.js
+++ src/data.js
@@ -1,8 +1,32 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
 
-window.example = example;
+// window.example = example
+
+const root = document.getElementById('root')
+
+
+const data = RICKANDMORTY.results
+let str = ''
+data.forEach(element => {
+  let name = element.name
+  let origin = element.origin.name
+  let species = element.species
+  let image = element.image
+  str +=  `<div class="fondo">
+  <h3 class = "prueba">${name}</h3>
+  <p class="prueba">${origin}</p>
+  <p>${species}</p>
+  <img src=${image} alt="">
+</div>`
+
+
+});
+
+
+
+root.innerHTML = str
