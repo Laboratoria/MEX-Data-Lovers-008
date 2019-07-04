2b86b20b83f7aae84e3a658c225b171db7b75124
thanya02 <tramirezm75@yahoo.com.mx>
2019-07-03 21:18:17 -0500

    Diseño de tarjetas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+for(personajes in RICKANDMORTY.results){
+  // console.log(RICKANDMORTY.results[personajes].image);
 
-for(personajes in RICKANDMORTY.results){ 
-  // console.log(RICKANDMORTY.results[personajes].image); 
-  //console.log(RICKANDMORTY.results[personajes].gender); 
+  
+  //console.log(RICKANDMORTY.results[personajes].gender);
+  
 
 }
 const data = RICKANDMORTY.results
@@ -11,27 +13,29 @@ const paraPintar = document.getElementById ('data');
 const muestraData = () => {
   let str = '';
   data.forEach(element => {
-    //console.log(element)
-    str += `<div class="datos">
+    console.log(element)
+    str += `<div class="tarjeta">
+    <div class="nombre">
+    <p> ${element.name}</p></div>
+    <br>
+    <div class="img"> 
     <img src="${element.image}"></img>
-    <p> ${element.name}</p>
+    </div>
+ <div class= "info">
+ 
     <p> Status: ${element.status} </p>
     <p> Especie: ${element.species} </p>
     <p> tamaño: ${element.origin.name} </p>
     <p> ${element.location.name}</p>
+    </div>
     </div>`
   });
+
   paraPintar.innerHTML = str;
+
 }
+
 muestraData()
- //printdata();
-
-const obtenerNames = data.map(function (data) {
- 
-  return data.name; 
-
-});
-console.log(obtenerNames);
-paraPintar.innerHTML = obtenerNames.toString(obtenerNames);
+// printdata();
 
 
