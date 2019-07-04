2db6786a7d7b61bed913d64b53e4c7b558448317
SolGee <solsv00@hotmail.com>
2019-07-04 13:47:41 -0500

    Header responsivo

--- src/data.js
+++ src/data.js
@@ -8,5 +8,20 @@ const filterData = (key, value, data) => { //2 parámetros. Value del index tien
     return items;
 };
 
+const orderData = (data) => {
+    let items = data.map(element => element.name)
+    items.sort();
+            // return items.reverse();
+        console.log(items.reverse());
+    }
+        //(a,b) => {
+       /*     return a-b
+        })
+        console.log(items);
+        return items
+    }*/
 
+window.orderData = orderData;
 window.filterData = filterData; //Guarda la función en el global para poder ser invocada de donde sea
+
+
