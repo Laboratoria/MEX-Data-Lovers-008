0d056cd0ccb9dad59a3c232bd918e2a7d1d3ae22
ushaHernandez <ursunina@hotmail.com>
2019-07-05 13:57:01 -0500

    Impresion de data

--- src/data.js
+++ src/data.js
@@ -1,12 +1,20 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+// traer data
+const data = RICKANDMORTY;
+console.log(data);
+const card = document.getElementById ("rick-mory-card")
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+const dataSee = (data)=>{
+  let str = ''
+    data.results.forEach(element => {
+      console.log(element )
+     str +=`<div class="card"><p>${element.name}<p>
+     <p>${element.status}<p>
+     <p> specie: ${element.species}<p> <p>${element.gender}<p><p>${element.origin.name}<p><p>${element.location.name}<p>
+     <img src = "${element.image}"> </di>`
 
-for (let i = 0; i < RICKANDMORTY.results.length; i++ ){
-  console.log (RICKANDMORTY.results[i].status)
+     
+   });
+   card.innerHTML = str
 }
+dataSee(data)
