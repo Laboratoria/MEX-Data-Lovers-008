1695d1781346e99c9165549e57a79f14e499f181
JesssicaZetina <jessicazetina37@gmail.com>
2019-07-09 20:38:31 -0500

    agregamos data a main

--- src/data.js
+++ src/data.js
@@ -8,53 +8,12 @@
 // window.example = example
 
 //descomentar para mostrar data//
-const root = document.getElementById('root')
-
-
-const data = RICKANDMORTY.results
-let str = ''
-data.forEach(element => {
-  let name = element.name
-  let origin = element.origin.name
-  let species = element.species
-  let image = element.image
-  let status = element.estatus
-  let gener = element.gener
-  str +=  `<div class="fondo">
-  <h3 class = "prueba">${name}</h3>
-  <p class="prueba">${origin}</p>
-  <p>${species}</p>
-  <img src=${image} alt="">
-  <p> ${status}</p>
-  <p> ${gener}</p>
-</div>`
-
-});
 
 //funcion para menu//
-root.innerHTML = str
 
-$(document).ready(main);
- 
-var contador = 1;
- 
-function main () {
-	$('.menu_bar').click(function(){
-		if (contador == 1) {
-			$('nav').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('nav').animate({
-				left: '-100%'
-			});
-		}
-	});
- 
-	// Mostramos y ocultamos submenus
-	$('.submenu').click(function(){
-		$(this).children('.children').slideToggle();
-	});
-}
+//const newArray= arr.filter(function(value){
+  //return value
+//}
+
+//)
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
-function GuardarValor() {
-document.getElementById("input-name").value.addEventListener("click", () => 
-document.getElementById("").value);
-}
+const root = document.getElementById('root')
+
+
+const data = RICKANDMORTY.results
+let str = ''
+data.forEach(element => {
+  let name = element.name
+  let origin = element.origin.name
+  let species = element.species
+  let image = element.image
+  let status = element.estatus
+  let gener = element.gener
+  str +=  `<div class="fondo">
+  <h3 class = "prueba">${name}</h3>
+  <p class="prueba">${origin}</p>
+  <p>${species}</p>
+  <img src=${image} alt="">
+  <p> ${status}</p>
+  <p> ${gener}</p>
+</div>`
+
+});
+root.innerHTML=str
+
+
+
 
