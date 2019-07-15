73d5f449a18c15e3d166ab2456b52b6ec0c05dbf
AdrianaRosas <salamipizza8909@gmail.com>
2019-07-14 20:45:42 -0500

    Ultimos cambios

--- src/data.js
+++ src/data.js
@@ -1,31 +1,43 @@
-let empezar= document.getElementsByClassName("img"); 
-for(let i = 0 ; i < empezar.length; i++){
-  empezar[i].addEventListener('click', function() {
-    mostrar(empezar[i])
-})
 
-  var mostrar = function(info) {
-    info.style.display = (info.style.display == 'none') ? 'block' : 'none';
+
+let tarjetas= document.getElementsByClassName("tarjeta"); //variable para localizar las tarjetas, las devuelve en un arreglo
+
+for(let i = 0 ; i < tarjetas.length; i++){ //recorre el nuevo arreglo, lo mide y le asugna un valor numérico
+
+  tarjetas[i].addEventListener('click', function() { //con la variable interna, se le asigna el evento "Click" a cada una de las tarjetas
+
+    let tarjeta = tarjetas[i] //cada tarjeta se guarda en una variable
+
+    mostrar(tarjeta) //Se ejecuta la función mostrar y se le pasa como parámetro la tarjeta
+
+  })
+
+
 }
 
-    window.onload = function () {
-      
-   
-    }
+function mostrar(tarjeta) { 
+  //console.dir(tarjeta) //.dir sirve para mostrar las propiedades dentro del elemento HTML
+let info = tarjeta.children[0].childNodes[7]; //se crea un avariable nueva que va a buscar dentro del elemento HTML "tarjeta" los "hijos" en busca del elemento info
+//AQUI hacker edition: for para cuando se seleccione otra imagen, desaparezca la info de la anterior se va a cambiar la propiedad display de todos los elementos a none y después la que quieres
+
+  info.style.display == 'none' ? info.style.display='block' : info.style.display='none'; //Se utiliza un operador ternario para desplegar la información al hacer click en la tarjeta
+  //console.log(info);
+  
+}
 
 
-//     let empezar= document.getElementsByClassName("img"); 
-// empezar.addEventListener("click", mostrar) 
+let selector = document.getElementById("filter"); //Variable para buscar en HTML el selector
+let filter= function() { 
+  let seleccion= getCleanedString(selector.value); //Variable para obtener el valor de la selección del selector
+  //console.log(seleccion);
+  for(let i = 0 ; i < tarjetas.length; i++){ //Ocultar tarjetas
+    tarjetas[i].style.display = 'none'
+  }
+  let tarjetasFiltradas= document.getElementsByClassName(seleccion); //Variable que busca las misma clase del valor del selector. Devuelve un arreglo
+  for(let i = 0 ; i < tarjetasFiltradas.length; i++){
+    tarjetasFiltradas[i].style.display = 'block' //Se recorre el arreglo y se muestra 
+  }
+}
+selector.addEventListener('change', filter); //Se llama al evento "change" que sirve para obtener el valor del selector cuando cambia
 
-// var mostrar = function() { 
-// //  alert('adsf')
-// if (document.getElementsByClassName){
-//   const activar = document.getElementsByClassName("info");
-//   activar.style.display = (activar.style.display == 'none') ? 'block' : 'none';
-// }
-// }
 
-//     window.onload = function () {
-      
-//     mostrar('info')
-    }
--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
 
-for(personajes in RICKANDMORTY.results){ 
-  // console.log(RICKANDMORTY.results[personajes].image); 
-  //console.log(RICKANDMORTY.results[personajes].gender); 
-
-for(personajes in RICKANDMORTY.results){
-  // console.log(RICKANDMORTY.results[personajes].image);
-
-
-for(personajes in RICKANDMORTY.results){
-  // console.log(RICKANDMORTY.results[personajes].image); 
-  //console.log(RICKANDMORTY.results[personajes].gender);
-
-}
-}
-}
-const data = RICKANDMORTY.results;
+const personajes = RICKANDMORTY.results;
 // const data RICKANDMORTY.results
-const paraPintar = document.getElementById ('data');
+const paraPintar = document.getElementById ('data'); 
+// console.log(personajes)
+//MOSTRAR 
 
-//filtrar data
+//Para eliminar espacios y caracteres
+function getCleanedString(filter){
+  // Definimos los caracteres que queremos eliminar
+  var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.ζáéíóúÁÉÍÓÚü´'" ;
+  
+  // Los eliminamos todos
+  for (var i = 0; i < specialChars.length; i++) {
+    filter= filter.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+  }   
+  
+  // Lo queremos devolver limpio en minusculas
+  filter = filter.toLowerCase();
+  
+  // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
+  filter = filter.replace(/ /g,"_");
+  console.log(filter);
+  return filter;
+}
 
 const muestraData = () => {
   let str = '';
-  data.forEach(element => {
-    str += `<div class="tarjeta">
+  personajes.forEach(element => {
+    //let species= getCleanedString(element.species);
+    let origin= getCleanedString(element.origin.name);
+    str += `<div class="tarjeta ${element.species} ${origin}">
     <div class= "caras">
     <div class="nombre">
     <p> ${element.name}</p></div>
@@ -31,7 +37,7 @@ const muestraData = () => {
     <div class="img"> 
     <img src="${element.image}"></img>
     </div>
- <div class= "info">
+ <div class= "info" style= "display: none">
     <p> Status: ${element.status} </p>
     <p> Especie: ${element.species} </p>
     <p> tamaño: ${element.origin.name} </p>
@@ -47,10 +53,35 @@ const muestraData = () => {
 muestraData();
 
 
-//Filtrar
+//FILTRAR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Filtrar
 const filtrarData = (prop, val) => {
   let str = '';
-  data.forEach(element => {
+  personajes.forEach(element => {
     
     //console.log(element[prop])
     
@@ -60,9 +91,9 @@ const filtrarData = (prop, val) => {
           <p> ${element.name}</p></div>
           <br>
           <div class="img"> 
-          <img src="${element.image}"></img>
+            <img src="${element.image}"></img>
           </div>
-          <div class= "info">
+          <div class= "info" style= "display: none">
           <p> Status: ${element.status} </p>
           <p> Especie: ${element.species} </p>
           <p> tamaño: ${element.origin.name} </p>
@@ -75,7 +106,7 @@ const filtrarData = (prop, val) => {
   });
 
 
-  console.log(str);
+  // console.log(str);
 
 }
-filtrarData('status', 'Alive')
+filtrarData('status', 'Alive')*/
