bf930a5a6b024d0c59b8a03d59fb7f0e7d6237ce
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-07-15 13:45:51 -0500

    subiendo cambios tarjetas, funciones, imagen carpeta

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 
-//filtrar por rol del campeón 
-
+const allAvatar =  document.getElementById('allAvatar')
 
 const roleBtn = document.getElementsByClassName('roleBtn');
 // console.log(roleBtn);
 for (let index = 0; index < roleBtn.length; index++) {
   roleBtn[index].addEventListener ('click', ()=> {
+    allAvatar.style.display = "none"
     const idTarget = event.target.id;
     const roleClick = newArray.filter(championRole => championRole.tags[0] == idTarget || championRole.tags[1] == idTarget);
     // console.log(roleClick);
@@ -14,5 +14,5 @@ for (let index = 0; index < roleBtn.length; index++) {
 }
 
 
-
 window.roleBtn = roleBtn
+
